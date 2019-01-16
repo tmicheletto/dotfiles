c1deb09deda139eb787a10957ef2582f786ef7ef
Tim Micheletto <tmicheletto@seek.com.au>
2019-01-17 10:16:30 +1100

    Add Brewfile

---
+++ Brewfile
@@ -0,0 +1,17 @@
+# Taps
+tap "homebrew/core"
+tap "homebrew/bundle"
+tap "homebrew/services"
+tap "caskroom/cask"
+
+# Binaries
+brew "coreutils"
+brew "adr-tools"
+brew "macvim --with-cscope --with-lua --override-system-vim"
+brew "go"
+
+# Apps
+cask "atom"
+cask "google-chrome"
+cask "slack"
+cask "macdown"
--- install.sh
+++ install.sh
@@ -9,24 +9,16 @@
 # Install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-# Install MacVim with LUA support
-brew install macvim --with-cscope --with-lua --override-system-vim
-
 # Install vimrc
 git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
 
-# Install Core utils for consistent tooling between Mac and Linux
-brew install coreutils
+# Homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-# Install MacDown
-brew cask install macdown
-
-# ADR Tools
-brew install adr-tools
-
-#Go
-brew install go
+#Brew bundle
+brew tap Homebrew/bundle
+brew bundle
 
 # Install Ammonite
 sudo sh -c '(echo "#!/usr/bin/env sh" && curl -L https://github.com/lihaoyi/Ammonite/releases/download/1.1.2/2.12-1.1.2) > /usr/local/bin/amm && chmod +x /usr/local/bin/amm' && amm
