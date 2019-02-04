a2d5630e248cef5e6acba3a5cbad71b55eb7c883
Tim Micheletto <tmicheletto@seek.com.au>
2019-02-05 10:53:48 +1100

    Critical stuff complete

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# dotfiles
+# dotfiles repo
+
+**Runsheet**
+1. Run `xcode-select --install` to install xcode command line tools (incl git)
+2. Run `/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"` to install homebrew
+3. Run `brew bundle` in the brew directory of this repo
+4. Run `./bootstrap.sh` from the root of this repo
--- bootstrap.sh
+++ bootstrap.sh
@@ -15,6 +15,7 @@ echo >&2 'Updating ~/.zshrc...'
 echo "source ${repo_home}/dots/zshrc" >> "${HOME}/.zshrc"
 
 # Link each of the dotfile repo files into the home directory
+echo "Linking dot files..."
 cd "${repo_home}/dots"
 for dot_file in !(zshrc); do
   rm -f "${HOME}/.${dot_file}" 2> /dev/null || true
@@ -22,19 +23,30 @@ for dot_file in !(zshrc); do
   ln -s "${repo_home}/dots/${dot_file}" "${HOME}/.${dot_file}"
 done
 
+# Install powerline fonts
+echo "Installing powerline fonts..."
+git clone https://github.com/powerline/fonts.git --depth=1 .fonts
+# install
+cd .fonts
+./install.sh
+# clean-up a bit
+cd ..
+rm -rf .fonts
+
 # Install oh-my-zsh
+echo "Installing oh-my-zsh..."
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # Install vimrc
-git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
-sh ~/.vim_runtime/install_awesome_vimrc.sh
-
-# Homebrew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
-#Brew bundle
-brew tap Homebrew/bundle
-brew bundle --file "${repo_home}/brew/Brewfile"
+echo "Installing vimrc..."
+if [ ! -d ~/.vim_runtime ];
+then
+  git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
+  sh ~/.vim_runtime/install_awesome_vimrc.sh
+else
+  echo "~/.vim_runtime directory exists. Skipping vimrc..."
+fi
 
 # Install Ammonite
+echo "Installng Ammonite..."
 sudo sh -c '(echo "#!/usr/bin/env sh" && curl -L https://github.com/lihaoyi/Ammonite/releases/download/1.1.2/2.12-1.1.2) > /usr/local/bin/amm && chmod +x /usr/local/bin/amm' && amm
