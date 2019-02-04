14db5d2adba1840a3cfbee55b4c1a72257a3427b
Tim Micheletto <tmicheletto@seek.com.au>
2019-02-04 17:39:41 +1100

    correct path to brewfile

--- bootstrap.sh
+++ bootstrap.sh
@@ -34,7 +34,7 @@ sh ~/.vim_runtime/install_awesome_vimrc.sh
 
 #Brew bundle
 brew tap Homebrew/bundle
-brew bundle
+brew bundle --file "${repo_home}/brew/Brewfile"
 
 # Install Ammonite
 sudo sh -c '(echo "#!/usr/bin/env sh" && curl -L https://github.com/lihaoyi/Ammonite/releases/download/1.1.2/2.12-1.1.2) > /usr/local/bin/amm && chmod +x /usr/local/bin/amm' && amm
