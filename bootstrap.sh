43d8ddab564a8ee26b1571f5f651b81d7a2de946
Tim Micheletto <tmicheletto@seek.com.au>
2020-04-07 14:23:53 +1000

    Refactor to prezto

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,20 +8,58 @@ shopt -s extglob
 # @author Tim Micheletto
 ###########################
 
+echo "Setting up your Mac..."
+
+# Check for Homebrew and install if we don't have it
+if test ! $(which brew); then
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+# Update Homebrew recipes
+brew update
+
+# Install all our dependencies with bundle (See Brewfile)
+brew tap homebrew/bundle
+brew bundle --file=brew/Brewfile --no-upgrade
+
+prezto_home="${HOME}/.zprezto"
 repo_home="$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)"
+
+# Make sure Prezto has been installed
+if [[ ! -d "${prezto_home}" ]]; then
+  git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+else
+  echo "Prezto already installed. Skipping..."
+fi
+
+# Clean out any Prezto changes and get latest
+cd "${prezto_home}"
+git checkout -- .
+git pull
+git submodule update --init --recursive
+
+# Delete existing Prezto-owned RC files and re-link
+cd "${prezto_home}/runcoms"
+for rc_file in z*; do
+  rm -f "${HOME}/.${rc_file}" 2> /dev/null || true
+  ln -s "${prezto_home}/runcoms/${rc_file}" "${HOME}/.${rc_file}"
+done
 
 # Update ~/.zshrc to source dotfile repo zshrc
 echo >&2 'Updating ~/.zshrc...'
 echo "source ${repo_home}/dots/zshrc" >> "${HOME}/.zshrc"
 
 # Link each of the dotfile repo files into the home directory
-echo "Linking dot files..."
 cd "${repo_home}/dots"
 for dot_file in !(zshrc); do
   rm -f "${HOME}/.${dot_file}" 2> /dev/null || true
   echo >&2 "Linking ~/.${dot_file}..."
   ln -s "${repo_home}/dots/${dot_file}" "${HOME}/.${dot_file}"
 done
+
+# Setup bin directory
+# mkdir -p "${HOME}/bin"
+# cp "${repo_home}"/bin/* "${HOME}/bin"
 
 # Install powerline fonts
 echo "Installing powerline fonts..."
@@ -33,10 +71,6 @@ cd .fonts
 cd ..
 rm -rf .fonts
 
-# Install oh-my-zsh
-echo "Installing oh-my-zsh..."
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
 # Install vimrc
 echo "Installing vimrc..."
 if [ ! -d ~/.vim_runtime ];
@@ -47,6 +81,12 @@ else
   echo "~/.vim_runtime directory exists. Skipping vimrc..."
 fi
 
-# Install Ammonite
-echo "Installng Ammonite..."
-sudo sh -c '(echo "#!/usr/bin/env sh" && curl -L https://github.com/lihaoyi/Ammonite/releases/download/1.1.2/2.12-1.1.2) > /usr/local/bin/amm && chmod +x /usr/local/bin/amm'
+# Import iterm preferences
+
+# Specify the preferences directory
+defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/dotfiles/iterm"
+# Tell iTerm2 to use the custom preferences in the directory
+defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
+
+# Set Zsh as default shell
+chsh -s /bin/zsh
